a2d1ada9d2f2ff441c3d0fdbef4516c0224c4e70
Greg Alexander <gitgreg@galexander.org>
2020-12-28 21:37:19 -0500

    Remove sources from the makefile that are not available in dropbear 2020.81 (will need to add some too, later).

--- jni/Android.mk
+++ jni/Android.mk
@@ -18,7 +18,6 @@ DROPBEAR_SRCS := $(DROPBEAR_PATH)/atomicio.c \
 	$(DROPBEAR_PATH)/common-session.c \
 	$(DROPBEAR_PATH)/compat.c \
 	$(DROPBEAR_PATH)/crypto_desc.c \
-	$(DROPBEAR_PATH)/curve25519-donna.c \
 	$(DROPBEAR_PATH)/dbhelpers.c \
 	$(DROPBEAR_PATH)/dbmalloc.c \
 	$(DROPBEAR_PATH)/dbrandom.c \
@@ -325,12 +324,6 @@ DROPBEAR_SRCS := $(DROPBEAR_PATH)/atomicio.c \
 	$(DROPBEAR_PATH)/libtomcrypt/src/prngs/sober128.c \
 	$(DROPBEAR_PATH)/libtomcrypt/src/prngs/sprng.c \
 	$(DROPBEAR_PATH)/libtomcrypt/src/prngs/yarrow.c \
-	$(DROPBEAR_PATH)/libtommath/bn_error.c \
-	$(DROPBEAR_PATH)/libtommath/bn_fast_mp_invmod.c \
-	$(DROPBEAR_PATH)/libtommath/bn_fast_mp_montgomery_reduce.c \
-	$(DROPBEAR_PATH)/libtommath/bn_fast_s_mp_mul_digs.c \
-	$(DROPBEAR_PATH)/libtommath/bn_fast_s_mp_mul_high_digs.c \
-	$(DROPBEAR_PATH)/libtommath/bn_fast_s_mp_sqr.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_2expt.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_abs.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_add.c \
@@ -355,28 +348,19 @@ DROPBEAR_SRCS := $(DROPBEAR_PATH)/atomicio.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_dr_reduce.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_dr_setup.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_exch.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_expt_d.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_expt_d_ex.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_exptmod.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_exptmod_fast.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_exteuclid.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_fread.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_fwrite.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_gcd.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_get_int.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_grow.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_init.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_init_copy.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_init_multi.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_init_set.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_init_set_int.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_init_size.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_invmod.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_invmod_slow.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_is_square.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_jacobi.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_karatsuba_mul.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_karatsuba_sqr.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_lcm.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_lshd.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_mod.c \
@@ -390,23 +374,17 @@ DROPBEAR_SRCS := $(DROPBEAR_PATH)/atomicio.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_mul_2d.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_mul_d.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_mulmod.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_n_root.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_n_root_ex.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_neg.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_or.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_fermat.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_is_divisible.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_is_prime.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_miller_rabin.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_next_prime.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_rabin_miller_trials.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_prime_random_ex.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_radix_size.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_radix_smap.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_rand.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_read_radix.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_read_signed_bin.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_read_unsigned_bin.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_reduce.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_reduce_2k.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_reduce_2k_l.c \
@@ -417,35 +395,22 @@ DROPBEAR_SRCS := $(DROPBEAR_PATH)/atomicio.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_reduce_setup.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_rshd.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_set.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_set_int.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_shrink.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_signed_bin_size.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_sqr.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_sqrmod.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_sqrt.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_sub.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_sub_d.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_submod.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_to_signed_bin.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_to_signed_bin_n.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_to_unsigned_bin.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_to_unsigned_bin_n.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_toom_mul.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_toom_sqr.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_toradix.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_toradix_n.c \
-	$(DROPBEAR_PATH)/libtommath/bn_mp_unsigned_bin_size.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_xor.c \
 	$(DROPBEAR_PATH)/libtommath/bn_mp_zero.c \
 	$(DROPBEAR_PATH)/libtommath/bn_prime_tab.c \
-	$(DROPBEAR_PATH)/libtommath/bn_reverse.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_add.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_exptmod.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_mul_digs.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_mul_high_digs.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_sqr.c \
 	$(DROPBEAR_PATH)/libtommath/bn_s_mp_sub.c \
-	$(DROPBEAR_PATH)/libtommath/bncore.c \
 	$(DROPBEAR_PATH)/list.c \
 	$(DROPBEAR_PATH)/listener.c \
 	$(DROPBEAR_PATH)/loginrec.c \
